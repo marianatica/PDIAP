e2e89d831a31ec0c0d3e5dba2204c53f88c7bedf
kallelrm <kallelr@outlook.com>
2017-09-22 20:27:09 -0300

    desativando a inscrição de projetos de novo

--- public/assets/js/routes/ui-routes.js
+++ public/assets/js/routes/ui-routes.js
@@ -39,11 +39,11 @@
 		})
 
 		// 	descomentar esse pra ativar a rota pra inscricao de projetos
-		 .state('inscricao', {
-		 	url: "/projetos/inscricao",
-		 	templateUrl: "/views/inscricao.html",
-		 	controller: "registroCtrl"
-		 })
+		//  .state('inscricao', {
+		//  	url: "/projetos/inscricao",
+		//  	templateUrl: "/views/inscricao.html",
+		//  	controller: "registroCtrl"
+		//  })
 
 
 		.state('regulamento', {
