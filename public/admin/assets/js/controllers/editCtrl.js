f46ed4d98cb1886a3a12c24abd169c3ae1f6392d
Nunes <guilherme.j.r.nunes@gmail.com>
2018-08-16 20:16:54 -0300

    Correção de erros

--- public/admin/assets/js/controllers/editCtrl.js
+++ public/admin/assets/js/controllers/editCtrl.js
@@ -37,50 +37,7 @@
 			})
 			.error(function(status) {
 				console.log('Error: '+status);
-			});/*success(function(data, status) {
-				if (data === 'success') {
-					let showConfirmDialog = function(ev) {
-						var confirm = $mdDialog.confirm()
-						.title('Parabéns!')
-						.textContent('Inscrição realizada com sucesso!')
-						.ariaLabel('Inscrição realizada com sucesso!')
-						.targetEvent(ev)
-						.ok('OK')
-						$mdDialog.show(confirm);
-					};
-					showConfirmDialog();
-					resetForm();
-				} else {
-					let showConfirmDialog = function(ev) {
-						var confirm = $mdDialog.confirm()
-						.title('Ops...')
-						.textContent('A inscrição não foi realizada. Por favor, tente novamente.')
-						.ariaLabel('A inscrição não foi realizada.')
-						.targetEvent(ev)
-						.theme('error')
-						.ok('OK');
-						$mdDialog.show(confirm);
-					};
-					showConfirmDialog();
-				}
-			})
-			.error(function(status) {
-				let showConfirmDialog = function(ev) {
-					var confirm = $mdDialog.confirm()
-					.title('Ops...')
-					.textContent('A inscrição não foi realizada. Por favor, tente novamente.')
-					.ariaLabel('A inscrição não foi realizada.')
-					.targetEvent(ev)
-					.theme('error')
-					.ok('OK');
-					$mdDialog.show(confirm);
-				};
-				showConfirmDialog();
-				console.log(status);
-			});*/
-
-
-/**/
+			});
 		}
 
 			
