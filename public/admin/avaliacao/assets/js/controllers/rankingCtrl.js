5d89fc79f0cf72272fbc91c35018e968885bacfb
Nunes <guilherme.j.r.nunes@gmail.com>
2018-09-26 14:29:39 -0300

    Ranking

--- public/admin/avaliacao/assets/js/controllers/rankingCtrl.js
+++ public/admin/avaliacao/assets/js/controllers/rankingCtrl.js
@@ -3,7 +3,7 @@
 
 	angular
 	.module('PDIAPav')
-	.controller('rankingCtrl', function($scope, $rootScope, $mdDialog, avaliacaoAPI) {
+	.controller('rankingCtrl', function($scope, $rootScope, $mdDialog, $filter, avaliacaoAPI) {
 
 		$rootScope.projetos = [];
 		$rootScope.eixo1_1 = [];
@@ -28,6 +28,9 @@
 			avaliacaoAPI.getTodosProjetos()
 			.success(function(projetos) {
 				angular.forEach(projetos, function (value, key) {
+					var ano = new Date(value.createdAt).getFullYear();
+					var ano_atual = new Date(Date.now()).getFullYear();
+					if(ano == ano_atual){
 					if (value.aprovado === true && (value.avaliacao !== undefined || value.participa === true)) {
 						if (value.categoria === 'Fundamental I (1º ao 5º anos)') {
 							if (value.avaliacao !== undefined && value.avaliacao.length > 0) {
@@ -258,7 +261,9 @@
 						});
 						$rootScope.trouxas.push(obj);
 					}
+                }
 				});
+                		$scope.reordenar();
 			})
 			.error(function(status) {
 				console.log(status);
@@ -324,5 +329,173 @@
 
 		carregarProjetos();
 
+		$scope.reordenar = function(){		
+			var eixo1_1 = [];
+			var eixo1_2 = [];
+			var eixo1_3 = [];
+			var eixo1_4 = [];
+
+			var eixo2_1 = [];
+			var eixo2_2 = [];
+			var eixo2_3 = [];
+			var eixo2_4 = [];
+
+			var eixo1 = [];
+			var eixo2 = [];
+			var eixo3 = [];
+			var eixo4 = [];
+			var eixo5 = [];
+			var eixo6 = [];
+			var eixo7 = [];
+			$rootScope.eixo1_1 = $filter('orderBy')($rootScope.eixo1_1,'-total',false);
+			$rootScope.eixo1_2 = $filter('orderBy')($rootScope.eixo1_2,'-total',false);
+			$rootScope.eixo1_3 = $filter('orderBy')($rootScope.eixo1_3,'-total',false);
+			$rootScope.eixo1_4 = $filter('orderBy')($rootScope.eixo1_4,'-total',false);
+
+			$rootScope.eixo2_1 = $filter('orderBy')($rootScope.eixo2_1,'-total',false);
+			$rootScope.eixo2_2 = $filter('orderBy')($rootScope.eixo2_2,'-total',false);
+			$rootScope.eixo2_3 = $filter('orderBy')($rootScope.eixo2_3,'-total',false);
+			$rootScope.eixo2_4 = $filter('orderBy')($rootScope.eixo2_4,'-total',false);
+
+			$rootScope.eixo1 = $filter('orderBy')($rootScope.eixo1,'-total',false);
+			$rootScope.eixo2 = $filter('orderBy')($rootScope.eixo2,'-total',false);
+			$rootScope.eixo3 = $filter('orderBy')($rootScope.eixo3,'-total',false);
+			$rootScope.eixo4 = $filter('orderBy')($rootScope.eixo4,'-total',false);
+			$rootScope.eixo5 = $filter('orderBy')($rootScope.eixo5,'-total',false);
+			$rootScope.eixo6 = $filter('orderBy')($rootScope.eixo6,'-total',false);
+			$rootScope.eixo7 = $filter('orderBy')($rootScope.eixo7,'-total',false);
+			for(var x=0; x<4;x++){
+				eixo1_1.push($rootScope.eixo1_1[x]);						
+				eixo1_2.push($rootScope.eixo1_2[x]);
+				eixo1_3.push($rootScope.eixo1_3[x]);
+				eixo1_4.push($rootScope.eixo1_4[x]);
+
+				eixo2_1.push($rootScope.eixo2_1[x]);						
+				eixo2_2.push($rootScope.eixo2_2[x]);
+				eixo2_3.push($rootScope.eixo2_3[x]);
+				eixo2_4.push($rootScope.eixo2_4[x]);
+		
+				eixo1.push($rootScope.eixo1[x]);
+				eixo2.push($rootScope.eixo2[x]);
+				eixo3.push($rootScope.eixo3[x]);
+				eixo4.push($rootScope.eixo4[x]);
+				eixo5.push($rootScope.eixo5[x]);
+				eixo6.push($rootScope.eixo6[x]);
+				eixo7.push($rootScope.eixo7[x]);
+			}
+
+			if(eixo1_1[3] == null) eixo1_1[3] = {_id:null,total:0};
+			if(eixo1_2[3] == null) eixo1_2[3] = {_id:null,total:0};
+			if(eixo1_3[3] == null) eixo1_3[3] = {_id:null,total:0};
+			if(eixo1_4[3] == null) eixo1_4[3] = {_id:null,total:0};
+
+			if(eixo2_1[3] == null) eixo1_1[3] = {_id:null,total:0};
+			if(eixo2_2[3] == null) eixo1_2[3] = {_id:null,total:0};
+			if(eixo2_3[3] == null) eixo1_3[3] = {_id:null,total:0};
+			if(eixo2_4[3] == null) eixo1_4[3] = {_id:null,total:0};
+
+			if(eixo1[3] == null) eixo1[3] = {_id:null,total:0};
+			if(eixo2[3] == null) eixo2[3] = {_id:null,total:0};
+			if(eixo3[3] == null) eixo3[3] = {_id:null,total:0};
+			if(eixo4[3] == null) eixo4[3] = {_id:null,total:0};
+			if(eixo5[3] == null) eixo5[3] = {_id:null,total:0};
+			if(eixo6[3] == null) eixo6[3] = {_id:null,total:0};
+			if(eixo7[3] == null) eixo7[3] = {_id:null,total:0};
+						
+			if(eixo1_1[2] == null) eixo1_1[2] = {_id:null,total:0};
+			if(eixo1_2[2] == null) eixo1_2[2] = {_id:null,total:0};
+			if(eixo1_3[2] == null) eixo1_3[2] = {_id:null,total:0};
+			if(eixo1_4[2] == null) eixo1_4[2] = {_id:null,total:0};
+
+			if(eixo2_1[2] == null) eixo1_1[2] = {_id:null,total:0};
+			if(eixo2_2[2] == null) eixo1_2[2] = {_id:null,total:0};
+			if(eixo2_3[2] == null) eixo1_3[2] = {_id:null,total:0};
+			if(eixo2_4[2] == null) eixo1_4[2] = {_id:null,total:0};
+
+			if(eixo1[2] == null) eixo1[2] = {_id:null,total:0};
+			if(eixo2[2] == null) eixo2[2] = {_id:null,total:0};
+			if(eixo3[2] == null) eixo3[2] = {_id:null,total:0};
+			if(eixo4[2] == null) eixo4[2] = {_id:null,total:0};
+			if(eixo5[2] == null) eixo5[2] = {_id:null,total:0};
+			if(eixo6[2] == null) eixo6[2] = {_id:null,total:0};
+			if(eixo7[2] == null) eixo7[2] = {_id:null,total:0};			
+
+			if(eixo1_1[1] == null) eixo1_1[1] = {_id:null,total:0};
+			if(eixo1_2[1] == null) eixo1_2[1] = {_id:null,total:0};
+			if(eixo1_3[1] == null) eixo1_3[1] = {_id:null,total:0};
+			if(eixo1_4[1] == null) eixo1_4[1] = {_id:null,total:0};
+
+			if(eixo2_1[1] == null) eixo1_1[1] = {_id:null,total:0};
+			if(eixo2_2[1] == null) eixo1_2[1] = {_id:null,total:0};
+			if(eixo2_3[1] == null) eixo1_3[1] = {_id:null,total:0};
+			if(eixo2_4[1] == null) eixo1_4[1] = {_id:null,total:0};
+
+			if(eixo1[1] == null) eixo1[1] = {_id:null,total:0};
+			if(eixo2[1] == null) eixo2[1] = {_id:null,total:0};
+			if(eixo3[1] == null) eixo3[1] = {_id:null,total:0};
+			if(eixo4[1] == null) eixo4[1] = {_id:null,total:0};
+			if(eixo5[1] == null) eixo5[1] = {_id:null,total:0};
+			if(eixo6[1] == null) eixo6[1] = {_id:null,total:0};
+			if(eixo7[1] == null) eixo7[1] = {_id:null,total:0};
+
+			if(eixo1_1[0] == null) eixo1_1 = [];
+			if(eixo1_2[0] == null) eixo1_2 = [];
+			if(eixo1_3[0] == null) eixo1_3 = [];
+			if(eixo1_4[0] == null) eixo1_4 = [];
+
+			if(eixo2_1[0] == null) eixo1_1 = [];
+			if(eixo2_2[0] == null) eixo1_2 = [];
+			if(eixo2_3[0] == null) eixo1_3 = [];
+			if(eixo2_4[0] == null) eixo1_4 = [];
+
+			if(eixo1[0] == null) eixo1 = [];
+			if(eixo2[0] == null) eixo2 = [];
+			if(eixo3[0] == null) eixo3 = [];
+			if(eixo4[0] == null) eixo4 = [];
+			if(eixo5[0] == null) eixo5 = [];
+			if(eixo6[0] == null) eixo6 = [];
+			if(eixo7[0] == null) eixo7 = [];
+
+			
+
+			$rootScope.eixo1_1 = $filter('orderBy')(eixo1_1,'nomeProjeto',false);
+			$rootScope.eixo1_2 = $filter('orderBy')(eixo1_2,'nomeProjeto',false);
+			$rootScope.eixo1_3 = $filter('orderBy')(eixo1_3,'nomeProjeto',false);
+			$rootScope.eixo1_4 = $filter('orderBy')(eixo1_4,'nomeProjeto',false);
+
+			$rootScope.eixo2_1 = $filter('orderBy')(eixo2_1,'nomeProjeto',false);
+			$rootScope.eixo2_2 = $filter('orderBy')(eixo2_2,'nomeProjeto',false);
+			$rootScope.eixo2_3 = $filter('orderBy')(eixo2_3,'nomeProjeto',false);
+			$rootScope.eixo2_4 = $filter('orderBy')(eixo2_4,'nomeProjeto',false);
+
+			$rootScope.eixo1 = $filter('orderBy')(eixo1,'nomeProjeto',false);
+			$rootScope.eixo2 = $filter('orderBy')(eixo2,'nomeProjeto',false);
+			$rootScope.eixo3 = $filter('orderBy')(eixo3,'nomeProjeto',false);
+			$rootScope.eixo4 = $filter('orderBy')(eixo4,'nomeProjeto',false);
+			$rootScope.eixo5 = $filter('orderBy')(eixo5,'nomeProjeto',false);
+			$rootScope.eixo6 = $filter('orderBy')(eixo6,'nomeProjeto',false);
+			$rootScope.eixo7 = $filter('orderBy')(eixo7,'nomeProjeto',false);
+		}
+
+		$scope.recarregar = function(filtro){
+			$rootScope.eixo1_1 = $filter('orderBy')($rootScope.eixo1_1,filtro,false);
+			$rootScope.eixo1_2 = $filter('orderBy')($rootScope.eixo1_2,filtro,false);
+			$rootScope.eixo1_3 = $filter('orderBy')($rootScope.eixo1_3,filtro,false);
+			$rootScope.eixo1_4 = $filter('orderBy')($rootScope.eixo1_4,filtro,false);
+
+			$rootScope.eixo2_1 = $filter('orderBy')($rootScope.eixo2_1,filtro,false);
+			$rootScope.eixo2_2 = $filter('orderBy')($rootScope.eixo2_2,filtro,false);
+			$rootScope.eixo2_3 = $filter('orderBy')($rootScope.eixo2_3,filtro,false);
+			$rootScope.eixo2_4 = $filter('orderBy')($rootScope.eixo2_4,filtro,false);
+
+			$rootScope.eixo1 = $filter('orderBy')($rootScope.eixo1,filtro,false);
+			$rootScope.eixo2 = $filter('orderBy')($rootScope.eixo2,filtro,false);
+			$rootScope.eixo3 = $filter('orderBy')($rootScope.eixo3,filtro,false);
+			$rootScope.eixo4 = $filter('orderBy')($rootScope.eixo4,filtro,false);
+			$rootScope.eixo5 = $filter('orderBy')($rootScope.eixo5,filtro,false);
+			$rootScope.eixo6 = $filter('orderBy')($rootScope.eixo6,filtro,false);
+			$rootScope.eixo7 = $filter('orderBy')($rootScope.eixo7,filtro,false);
+		}
+        
 	});
 })();
